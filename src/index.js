a5c05af53108e418a2e256e3e56a096f8ac98415
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2018-09-05 22:30:55 -0400

    add state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAxjrg3C8H_O9_y92kOhxekQ8AZz6I3ofo';
@@ -11,6 +10,6 @@ const App = () => {
       <SearchBar />
     </div>
   );
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
